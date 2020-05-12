b3b70dff896ce91592733898b0dd4dc902e31b68
raeyo <raeyo.kang@gmail.com>
2020-05-12 22:15:22 +0900

    refact: Edit main.py to detail project
    devide main script
    - logger region: logger setting
    - extract cad information
    - assemble

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from os.path import join, isfile, isdir
 from os import listdir
 from fileApi import get_dir_list, get_file_list, save_dic_to_yaml
-from extract_infoApi import initialize_furniture_config
+from extract_infoApi import initialize_furniture_config, load_instruction_info, initialize_assembly_status
 import logging
 
 
@@ -15,12 +15,42 @@ STEP_FILE_DIR = join(INPUT_PATH, "step_file")
 FURNITURE_NAME = "STEFAN"
 
 if __name__ == "__main__":
+    #---------------------------------------
+    #region logger
+    
     logger = logging.getLogger(FURNITURE_NAME)
     formatter = logging.Formatter('[%(asctime)s][%(levelname)s|%(filename)s:%(lineno)s] >> %(message)s')
     streamHandler = logging.StreamHandler()
     streamHandler.setFormatter(formatter)
     logger.addHandler(streamHandler)
+    logger.setLevel(level=logging.INFO)
     
-    logger.info("Initialize {} information".format(FURNITURE_NAME))
-    initialize_furniture_config(FURNITURE_NAME)
+    #endregion
+    
+    #---------------------------------------
+    #region initialize
+    
+    initialize_furniture_config(FURNITURE_NAME, logger)
+
+    #endregion
+
+    #---------------------------------------
+    #region assembly for each instruction
+    
+    instruction_step = 1
+    initialize_assembly_status(FURNITURE_NAME, instruction_step, logger)
+    instruction = load_instruction_info(instruction_step=instruction_step)
+    for part_name in instrcution.keys():
+        quantity = instruction[part_name]["quantity"] 
+        for idx in range(quantity):
+            instance_name = part_name + "_" + str(idx)
+            print(instance_name)
+
+    #endregion
+
+    #---------------------------------------
+    #region extract assembly sequence
+
+
+    #endregion
     
