26de1eb4b0c244b4d8b95cac8c39c5b95c81fb7c
joosoon <31118416+joosoon@users.noreply.github.com>
2020-11-09 19:25:40 +0900

    Add files via upload

---
+++ sequence_joo.py
@@ -0,0 +1,458 @@
+sequence_1 = [
+    {
+        0: {
+            "part_name": "ikea_stefan_bracket",
+            "instnace_id": 1,
+            "assembly_point": 0
+        },
+        1: {
+            "part_name": "ikea_stefan_long",
+            "instnace_id": 0,
+            "assembly_point": 7
+        }
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_bracket",
+            "instnace_id": 0,
+            "assembly_point": 0
+        },
+        1: {
+            "part_name": "ikea_stefan_long",
+            "instnace_id": 0,
+            "assembly_point": 6
+        }
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_bracket",
+            "instnace_id": 2,
+            "assembly_point": 0
+        },
+        1: {
+            "part_name": "ikea_stefan_short",
+            "instnace_id": 0,
+            "assembly_point": 7
+        }
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_bracket",
+            "instnace_id": 3,
+            "assembly_point": 0
+        },
+        1: {
+            "part_name": "ikea_stefan_short",
+            "instnace_id": 0,
+            "assembly_point": 6
+        }
+    }
+]
+sequence_2 = [
+    {
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 0,
+            "assembly_point": 0
+        },
+        1: {
+            "part_name": "ikea_stefan_short",
+            "instnace_id": 0,
+            "assembly_point": 0
+        }
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 2,
+            "assembly_point": 0
+        },
+        1: {
+            "part_name": "ikea_stefan_short",
+            "instnace_id": 0,
+            "assembly_point": 2
+        }
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 1,
+            "assembly_point": 0
+        },
+        1: {
+            "part_name": "ikea_stefan_short",
+            "instnace_id": 0,
+            "assembly_point": 3
+        }
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 3,
+            "assembly_point": 0
+        },
+        1: {
+            "part_name": "ikea_stefan_short",
+            "instnace_id": 0,
+            "assembly_point": 5
+        }
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 1,
+            "assembly_point": 1
+        },
+        1: {
+            "part_name": "ikea_stefan_left",
+            "instnace_id": 0,
+            "assembly_point": 8
+        }
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 3,
+            "assembly_point": 1
+        },
+        1: {
+            "part_name": "ikea_stefan_left",
+            "instnace_id": 0,
+            "assembly_point": 9
+        }
+    },
+]
+sequence_3 = [
+    {
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 4,
+            "assembly_point": 0
+        },
+        1: {
+            "part_name": "ikea_stefan_long",
+            "instnace_id": 0,
+            "assembly_point": 5
+        }
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 5,
+            "assembly_point": 0
+        },
+        1: {
+            "part_name": "ikea_stefan_long",
+            "instnace_id": 0,
+            "assembly_point": 3
+        }
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 7,
+            "assembly_point": 0
+        },
+        1: {
+            "part_name": "ikea_stefan_long",
+            "instnace_id": 0,
+            "assembly_point": 2
+        }
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 6,
+            "assembly_point": 0
+        },
+        1: {
+            "part_name": "ikea_stefan_long",
+            "instnace_id": 0,
+            "assembly_point": 0
+        }
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 7,
+            "assembly_point": 1
+        },
+        1: {
+            "part_name": "ikea_stefan_left",
+            "instnace_id": 0,
+            "assembly_point": 2
+        }
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 6,
+            "assembly_point": 1
+        },
+        1: {
+            "part_name": "ikea_stefan_left",
+            "instnace_id": 0,
+            "assembly_point": 1
+        }
+    },
+]
+sequence_4 = [
+    {
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 13,
+            "assembly_point": 0
+        },
+        1: {
+            "part_name": "ikea_stefan_middle",
+            "instnace_id": 0,
+            "assembly_point": 1
+        }
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 10,
+            "assembly_point": 0
+        },
+        1: {
+            "part_name": "ikea_stefan_middle",
+            "instnace_id": 0,
+            "assembly_point": 0
+        }
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 11,
+            "assembly_point": 0
+        },
+        1: {
+            "part_name": "ikea_stefan_middle",
+            "instnace_id": 0,
+            "assembly_point": 6
+        }
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 8,
+            "assembly_point": 0
+        },
+        1: {
+            "part_name": "ikea_stefan_middle",
+            "instnace_id": 0,
+            "assembly_point": 4
+        }
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 9,
+            "assembly_point": 0
+        },
+        1: {
+            "part_name": "ikea_stefan_middle",
+            "instnace_id": 0,
+            "assembly_point": 3
+        }
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 12,
+            "assembly_point": 0
+        },
+        1: {
+            "part_name": "ikea_stefan_middle",
+            "instnace_id": 0,
+            "assembly_point": 7
+        }
+    },
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 8,
+            "assembly_point": 1
+        },
+        1: {
+            "part_name": "ikea_stefan_left",
+            "instnace_id": 0,
+            "assembly_point": 5
+        }
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 9,
+            "assembly_point": 1
+        },
+        1: {
+            "part_name": "ikea_stefan_left",
+            "instnace_id": 0,
+            "assembly_point": 6
+        }
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 12,
+            "assembly_point": 1
+        },
+        1: {
+            "part_name": "ikea_stefan_left",
+            "instnace_id": 0,
+            "assembly_point": 7
+        }
+    },
+]
+sequence_5 = [
+    {
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 13,
+            "assembly_point": 1
+        },
+        1: {
+            "part_name": "ikea_stefan_right",
+            "instnace_id": 0,
+            "assembly_point": 5
+        }
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 10,
+            "assembly_point": 1
+        },
+        1: {
+            "part_name": "ikea_stefan_right",
+            "instnace_id": 0,
+            "assembly_point": 6
+        }
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 11,
+            "assembly_point": 1
+        },
+        1: {
+            "part_name": "ikea_stefan_right",
+            "instnace_id": 0,
+            "assembly_point": 7
+        }
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 2,
+            "assembly_point": 1
+        },
+        1: {
+            "part_name": "ikea_stefan_right",
+            "instnace_id": 0,
+            "assembly_point": 9
+        }
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 0,
+            "assembly_point": 1
+        },
+        1: {
+            "part_name": "ikea_stefan_right",
+            "instnace_id": 0,
+            "assembly_point": 8
+        }
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 4,
+            "assembly_point": 1
+        },
+        1: {
+            "part_name": "ikea_stefan_right",
+            "instnace_id": 0,
+            "assembly_point": 2
+        }
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_pin",
+            "instnace_id": 5,
+            "assembly_point": 1
+        },
+        1: {
+            "part_name": "ikea_stefan_right",
+            "instnace_id": 0,
+            "assembly_point": 1
+        }
+    },
+    # stefan bolt side
+    # 1) 한번에 3개씩 표현
+    # 2) bolt-right , bolt-middle 처럼 2개씩 나눠서 표현
+    {
+        0: {
+            "part_name": "ikea_stefan_bolt_side",
+            "instnace_id": 0,
+            "assembly_point": 0
+        },
+        1: {
+            "part_name": "ikea_stefan_right",
+            "instnace_id": 0,
+            "assembly_point": 4
+        },
+        2: {
+            "part_name": "ikea_stefan_middle",
+            "instnace_id": 0,
+            "assembly_point": 5
+        }
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_bolt_side",
+            "instnace_id": 0,
+            "assembly_point": 0
+        },
+        1: {
+            "part_name": "ikea_stefan_right",
+            "instnace_id": 0,
+            "assembly_point": 4
+        },
+    },
+    {
+        0: {
+            "part_name": "ikea_stefan_bolt_side",
+            "instnace_id": 0,
+            "assembly_point": 0
+        },
+        1: {
+            "part_name": "ikea_stefan_middle",
+            "instnace_id": 0,
+            "assembly_point": 5
+        }
+    },
+]
+sequence_6 = []
+sequence_9 = [    
+    {
+        0: {
+            "part_name": None,
+            "instnace_id": None,
+            "assembly_point": None
+        },
+        1: {
+            "part_name": None,
+            "instnace_id": None,
+            "assembly_point": None
+        }
+    },
+]
+
+
+
+
+        
+
